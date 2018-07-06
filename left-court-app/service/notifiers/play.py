83c93bed1bd0213700ab1359b1bf50d9906600e4
lhenrique <luiz.henrique@watchguard.com>
2018-07-06 16:37:42 -0300

    Added receiving method, now need to test.

--- left-court-app/service/notifiers/play.py
+++ left-court-app/service/notifiers/play.py
@@ -1,18 +1,35 @@
 import pika
 
-
 class PlayNotifier(object):
     def __init__(self,):
         self.host = 'localhost'
         self.connection = pika.BlockingConnection(pika.ConnectionParameters(self.host))
+        self.channel = self.connection.channel()
 
     def publish_play(self, play: dict, gameIdentifier: int):
-        exchange = "plays-game-{}-exchange".format(gameIdentifier)
-        routing_key = 'plays-python-{}'.format(gameIdentifier)
+        exchange = 'plays-exchange'.format(gameIdentifier)
+        routing_key = 'left-side-plays'.format(gameIdentifier)
         try:
-            self.connection.channel().basic_publish(exchange=exchange,
+            self.channel.basic_publish(exchange=exchange,
                                                     routing_key=routing_key,
                                                     body=play)
         except ConnectionError:
             print("DAMN YOU WABBIT!")
         print("[x] Sent Score to Queue")
+
+    def receive_play(self):
+        while self.channel.is_open:
+            try:
+                self.channel.basic_consume(queue='right-side-plays',consumer_callback=on_receive_play_callback)
+            except pika.exceptions.ConnectionClosedByBroker:
+                print('Connection closed by broker')
+                break
+            except pika.exceptions.AMQPChannelError:
+                print('Channel error on AMQP')
+                break
+            except pika.exceptions.AMQPConnectionError:
+                print('Connection error, retrying...')
+                continue
+
+
+
